f143ebe1c67c2b8705ef41776636306f67c06d74
rafighajili <rafig.hajili@gmail.com>
2024-03-31 12:04:01 +0400

    some changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,9 +5,8 @@ import { useTheme } from "next-themes";
 import { Button } from "#/ui/button";
 import { useEffect, useState } from "react";
 import { useSelectedLayoutSegment } from "next/navigation";
-import { GitHubLogoIcon, Half2Icon, HamburgerMenuIcon, MagnifyingGlassIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { GitHubLogoIcon, Half2Icon, HamburgerMenuIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { DialogTrigger, MenuTrigger } from "react-aria-components";
-import { Modal, ModalBody, ModalFooter, ModalHeader } from "#/ui/modal";
 import { Popover } from "#/ui/popover";
 import { twMerge } from "tailwind-merge";
 import { Menu, MenuItem } from "#/ui/menu";
@@ -49,7 +48,7 @@ export function Header() {
           Pigment UI
         </NextLink>
 
-        <div className="flex gap-x-4 max-sm:hidden">{routes}</div>
+        <div className="flex gap-x-2 max-sm:hidden">{routes}</div>
 
         <div className="ml-auto flex gap-x-2">
           <Button asChild isIconOnly variant="faded">
@@ -78,36 +77,5 @@ export function Header() {
         </div>
       </div>
     </header>
-  );
-}
-
-function SearchButton() {
-  return (
-    <DialogTrigger>
-      <Button
-        aria-label="General search"
-        variant="faded"
-        isIconOnly
-        startContent={<MagnifyingGlassIcon />}
-        endContent="C+K"
-        className="text-default-700"
-      >
-        <div className="w-32 text-start">General Search...</div>
-      </Button>
-      <Modal size="sm">
-        <ModalHeader>Heading</ModalHeader>
-        <ModalBody>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aut commodi consequatur cupiditate ducimus ex expedita hic illum
-            impedit ipsum itaque minima nemo, neque nisi, nobis non obcaecati officiis perspiciatis, porro quam quod saepe ullam veniam! Cupiditate
-            delectus incidunt maxime molestiae placeat quis recusandae sint ullam? Dolor fuga iste pariatur.
-          </p>
-        </ModalBody>
-        <ModalFooter className="flex justify-end gap-4">
-          <Button>Click Me</Button>
-          <Button autoFocus>Don&apos;t Click Me</Button>
-        </ModalFooter>
-      </Modal>
-    </DialogTrigger>
   );
 }
