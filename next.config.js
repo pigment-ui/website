d2ac086d2e19dbb4b196e94ee55783e7ec07bdf9
rafighajili <rafig.hajili@gmail.com>
2024-05-02 11:58:45 +0400

    some changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-const { withContentlayer } = require("next-contentlayer");
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ["react-aria", "react-aria-components"],
+  },
+  images: {
+    // cache optimized images for 60 seconds
+    minimumCacheTTL: 60,
+  },
+};
 
-module.exports = withContentlayer(nextConfig);
+export default nextConfig;
