0a37f280048410e51432a5fc65892d6e3bc9cc75
rafighajili <rafig.hajili@gmail.com>
2024-08-25 22:52:31 +0400

    updates

--- src/preview/button/radius.tsx
+++ src/preview/button/radius.tsx
@@ -4,11 +4,11 @@ import { Button } from "#/ui";
 function ButtonRadius() {
   return (
     <div className="flex flex-wrap gap-4">
+      <Button radius="none">Click me</Button>
       <Button radius="sm">Click me</Button>
       <Button radius="md">Click me</Button>
       <Button radius="lg">Click me</Button>
       <Button radius="full">Click me</Button>
-      <Button radius="none">Click me</Button>
     </div>
   );
 }
