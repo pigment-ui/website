bbdb1eddb26e787223638c4e7dfb6a2a78bafda5
rafighajili <rafig.hajili@gmail.com>
2025-03-09 11:53:33 +0100

    updates

--- packages/ui/src/checkbox.tsx
+++ packages/ui/src/checkbox.tsx
@@ -1,16 +1,38 @@
 "use client";
 
-import { useCheckboxGroupSlots } from "./checkbox-group";
+import { Field, FieldBaseProps } from "./field";
 import { isDisabledVariants, isFocusVisibleVariants, smallRadiusVariants, variantColorStyles } from "./styles";
 import { ColorProps, RadiusProps, SizeProps, StyleSlotsToStyleProps, VariantProps } from "./types";
+import { createSlots } from "./utils";
 import { CheckIcon, MinusIcon } from "lucide-react";
 import React, { ComponentPropsWithoutRef, ForwardedRef, forwardRef } from "react";
-import { mergeProps } from "react-aria";
-import { Checkbox as AriaCheckbox, CheckboxProps as AriaCheckboxProps, composeRenderProps, Link } from "react-aria-components";
+import { mergeProps, Orientation } from "react-aria";
+import {
+  Checkbox as AriaCheckbox,
+  CheckboxGroupProps as AriaCheckboxGroupProps,
+  CheckboxProps as AriaCheckboxProps,
+  composeRenderProps,
+  Link,
+} from "react-aria-components";
 import { twMerge } from "tailwind-merge";
 import { tv } from "tailwind-variants";
 
 // styles
+
+const checkboxGroupStyles = tv({
+  base: "flex",
+  variants: {
+    size: {
+      sm: "gap-1.5 py-1.5",
+      md: "gap-2 py-2",
+      lg: "gap-2.5 py-2.5",
+    },
+    orientation: {
+      horizontal: "flex-row",
+      vertical: "flex-col",
+    },
+  },
+});
 
 const checkboxStyles = tv({
   extend: variantColorStyles,
@@ -35,6 +57,12 @@ type CheckboxStylesReturnType = ReturnType<typeof checkboxStyles>;
 
 // props
 
+interface CheckboxGroupProps extends AriaCheckboxGroupProps, FieldBaseProps, VariantProps, ColorProps, RadiusProps {
+  orientation?: Orientation;
+  itemClassNames?: ComponentPropsWithoutRef<typeof Checkbox>["classNames"];
+  itemStyles?: ComponentPropsWithoutRef<typeof Checkbox>["styles"];
+}
+
 interface CheckboxProps
   extends AriaCheckboxProps,
     VariantProps,
@@ -43,7 +71,31 @@ interface CheckboxProps
     RadiusProps,
     StyleSlotsToStyleProps<CheckboxStylesReturnType> {}
 
+// slots
+
+interface CheckboxGroupSlotsType extends Pick<CheckboxGroupProps, "variant" | "color" | "size" | "radius" | "itemClassNames" | "itemStyles"> {}
+
+const [CheckboxGroupSlotsProvider, useCheckboxGroupSlots] = createSlots<CheckboxGroupSlotsType>();
+
 // component
+
+function _CheckboxGroup(props: CheckboxGroupProps, ref: ForwardedRef<HTMLInputElement>) {
+  const { variant = "soft", color = "default", size = "md", radius = size, orientation = "vertical", itemClassNames, itemStyles } = props;
+
+  return (
+    <CheckboxGroupSlotsProvider value={{ variant, color, size, radius, itemClassNames, itemStyles }}>
+      <AriaCheckboxGroup ref={ref} {...props}>
+        {composeRenderProps(props.children, (children, renderProps) => (
+          <Field {...renderProps} {...props}>
+            <div className={checkboxGroupStyles({ size, orientation })}>{children}</div>
+          </Field>
+        ))}
+      </AriaCheckboxGroup>
+    </CheckboxGroupSlotsProvider>
+  );
+}
+
+const CheckboxGroup = forwardRef(_CheckboxGroup);
 
 function _Checkbox(props: CheckboxProps, ref: ForwardedRef<HTMLLabelElement>) {
   const {
@@ -115,5 +167,4 @@ const CheckboxLink = forwardRef(_CheckboxLink);
 
 // exports
 
-export { Checkbox, CheckboxLink, checkboxStyles };
-export type { CheckboxStylesReturnType };
+export { CheckboxGroup, Checkbox, CheckboxLink };
